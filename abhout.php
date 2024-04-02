88705e2c1c2eea95de0e64d41683492a99c4a61c
nirmitiparkhe <165829313+nirmitiparkhe@users.noreply.github.com>
2024-04-02 23:33:16 +0530

    Add files via upload

---
+++ abhout.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>About Us - EduWeb</title>
+<link rel="stylesheet" href="style2.css">
+<style>
+/* CSS styles can be added here */
+body {
+font-family: cursive;
+line-height: 2.6;
+margin: 0;
+padding: 0;
+}
+header {
+background-color:#8594e4;
+color: #fff;
+padding: 10px 0;
+text-align: center;
+}
+
+.container {
+max-width: 800px;
+margin: 20px auto;
+padding: 0 20px;
+}
+</style>
+</head>
+<body>
+<header>
+<h1>About Us - EduWeb</h1>
+</header>
+<div class="container">
+<h2>Welcome to EduWeb!</h2>
+<p>EduWeb is online education platform dedicated to providing high-quality learning
+resources for students worldwide. Our mission is to make education accessible and enjoyable
+for everyone, regardless of their background or location.</p>
+<p>This website provides education resource for the following topics:
+<ol>
+<li>HTML</li>
+<li>CSS</li>
+<li>JAVASCRIPT</li>
+</ol>
+</p>
+<h3>Our Team</h3>
+<p>At EduWeb, we are proud to have a team of students who are passionate about
+education and technology.
+Our team members are SUJATA JAMADAR,NIRMITI PARKHE,SHRUTI KHARADE and
+TANISHA RAJWAR.
+</p>
+<h3>Our Vision</h3>
+<p>Our vision at EduWeb is to revolutionize the way people learn by harnessing the power
+of technology. We believe that by combining innovative tools and resources with expert
+knowledge, we can empower individuals to achieve their full potential and make a positive
+impact on the world.</p>
+<h3>Contact Us</h3>
+<p>If you have any questions, suggestions, or feedback, please feel free to contact us
+.<br>
+eduweb@gmail.com
+</p>
+<p>We would love to hear from you!</p>
+</div>
+</body>
+</html>
